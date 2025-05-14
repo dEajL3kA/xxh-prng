625e1a8343f83b1bf1c73d805da3ee5ab6d1b5fa
dEajL3kA <Cumpoing79@web.de>
2025-05-14 10:46:36 +0200

    Added ARM64 configuration to MSVC project file.

--- deploy.cmd
+++ deploy.cmd
@@ -4,8 +4,15 @@ setlocal EnableDelayedExpansion
 cd /d "%~d0"
 
 if "%VS2022INSTALLDIR%" == "" (
-	set "VS2022INSTALLDIR=C:\Program Files\Microsoft Visual Studio\2022\Community"
+	for %%e in (Community, Professional) do (
+		if exist "C:\Program Files\Microsoft Visual Studio\2022\%%e\VC\Auxiliary\Build\vcvars64.bat" (
+			set "VS2022INSTALLDIR=C:\Program Files\Microsoft Visual Studio\2022\%%e"
+			goto:exit_loop
+		)
+	)
 )
+
+:exit_loop
 
 if not exist "%VS2022INSTALLDIR%\VC\Auxiliary\Build\vcvars64.bat" (
 	echo Microsoft Visual C++ not found ^^!^^!^^!
@@ -18,7 +25,7 @@ for %%d in (bin obj) do (
 	if exist "%CD%/%%d/" rmdir /S /Q "%CD%/%%d"
 )
 
-for %%p in (x86 x64) do (
+for %%p in (x86 x64 ARM64) do (
 	msbuild /property:Platform=%%p /property:Configuration=Release /target:Rebuild "%CD%\xxh_rand.sln"
 	if !ERRORLEVEL! neq 0 (
 		echo.
--- run_tests.cmd
+++ run_tests.cmd
@@ -3,12 +3,12 @@ setlocal EnableDelayedExpansion
 
 cd /d "%~dp0"
 
-if not exist "%CD%\bin\x64\Release\xxh_rand.exe" (
+if not exist "%CD%\bin\x64\Release\xxh_rand-x64.exe" (
 	echo Error: Must build xxh_rand.exe before running the tests ^^!^^!^^!
 	pause && goto:eof
 )
 
-"%CD%\bin\x64\Release\xxh_rand.exe" | "%CD%\etc\tests\tools\win32\dieharder_x64.exe" -g 200 -a -k 2 -Y 1
+"%CD%\bin\x64\Release\xxh_rand-x64.exe" | "%CD%\etc\tests\tools\win32\dieharder_x64.exe" -g 200 -a -k 2 -Y 1
 
 echo.
 echo Test finished.
